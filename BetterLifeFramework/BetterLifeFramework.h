3334916349612eda8adbb5381cf43b40316a27c4
hrool20 <hugo_rosol@hotmail.com>
2018-07-28 12:29:13 -0500

    Initial Commit

---
+++ BetterLifeFramework/BetterLifeFramework.h
@@ -0,0 +1,19 @@
+//
+//  BetterLifeFramework.h
+//  BetterLifeFramework
+//
+//  Created by Hugo Andres on 28/07/18.
+//  Copyright © 2018 UPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BetterLifeFramework.
+FOUNDATION_EXPORT double BetterLifeFrameworkVersionNumber;
+
+//! Project version string for BetterLifeFramework.
+FOUNDATION_EXPORT const unsigned char BetterLifeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterLifeFramework/PublicHeader.h>
+
+
